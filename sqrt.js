e51fe0d71307b60f21d1951835b082c4c4ae5f4c
karthickofficl <karthickrajadev4@gmail.com>
2025-09-22 23:46:11 +0530

    first commit

---
+++ sqrt.js
@@ -0,0 +1,10 @@
+// sqrt 
+const number1 = 16;
+
+const findSquareRoot = Math.sqrt(number1);
+console.log(`The square root of ${number1} is ${findSquareRoot}`);
+// Output: The square root of 16 is 4
+
+// if suppose pass value is negative or string, it will return NaN
+const number2 = -16;
+const number3 = "hello";
